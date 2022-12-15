467bc4dbe18b26992aa12f5ce67f58e2119589af
flecoqui <flecoqui@hotmail.com>
2022-12-15 08:56:03 +0100

    update terraform file

--- infrastructure/main/main.tf
+++ infrastructure/main/main.tf
@@ -1,9 +1,9 @@
 locals {
- deploy_resource_group_name = format("%s/%s",var.resource_group_name,var.environment) 
+  deploy_resource_group_name = format("%s/%s", var.resource_group_name, var.environment)
 }
 
 data "azurerm_client_config" "current" {}
- 
+
 #Create Resource Group
 resource "azurerm_resource_group" "resourcegroup" {
   name     = local.deploy_resource_group_name
